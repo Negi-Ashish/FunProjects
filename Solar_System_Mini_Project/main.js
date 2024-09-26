0c6976d5eefd78127815379b582933cc840d9476
DONTKILLME <ashishnegi0511@gmail.com>
2024-09-26 23:41:53 +0530

    SunEarthMoon

--- Solar_System_Mini_Project/main.js
+++ Solar_System_Mini_Project/main.js
@@ -8,7 +8,89 @@ const pane = new Pane();
 // initialize the scene
 const scene = new THREE.Scene();
 
-// add stuff here
+// Texture
+const textureLoader = new THREE.TextureLoader();
+
+const sunTexture = textureLoader.load("/textures/2k_sun.jpg");
+sunTexture.colorSpace = THREE.SRGBColorSpace;
+
+const earthTexture = textureLoader.load("/textures/2k_earth_daymap.jpg");
+earthTexture.colorSpace = THREE.SRGBColorSpace;
+
+const moonTexture = textureLoader.load("/textures/2k_moon.jpg");
+moonTexture.colorSpace = THREE.SRGBColorSpace;
+
+// Materials
+const sunMaterial = new THREE.MeshBasicMaterial({
+  map: sunTexture,
+});
+
+const earthMaterial = new THREE.MeshStandardMaterial({
+  map: earthTexture,
+});
+const moonMaterial = new THREE.MeshStandardMaterial({
+  map: moonTexture,
+});
+
+// Geometry
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+
+// Mesh
+const sun = new THREE.Mesh(sphereGeometry, sunMaterial);
+sun.scale.setScalar(5);
+scene.add(sun);
+
+const planets = [
+  {
+    name: "Earth",
+    radius: 1,
+    distance: 20,
+    speed: 0.005,
+    material: earthMaterial,
+    moons: [
+      {
+        name: "Moon",
+        radius: 0.3,
+        distance: 3,
+        speed: 0.015,
+      },
+    ],
+  },
+];
+
+const createPlanet = (planet) => {
+  // Functions to create the Mesh's
+  const planetMesh = new THREE.Mesh(sphereGeometry, planet.material);
+  planetMesh.scale.setScalar(planet.radius);
+  planetMesh.position.x = planet.distance;
+  return planetMesh;
+};
+
+const createMoon = (moon) => {
+  // Functions to create the Mesh's
+  const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+  moonMesh.scale.setScalar(moon.radius);
+  moonMesh.position.x = moon.distance;
+  return moonMesh;
+};
+
+const planetMeshes = planets.map((planet) => {
+  const planetMesh = createPlanet(planet);
+  scene.add(planetMesh);
+
+  planet.moons.forEach((moon) => {
+    const moonMesh = createMoon(moon);
+    planetMesh.add(moonMesh);
+  });
+  return planetMesh;
+});
+
+// add lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 1000);
+scene.add(pointLight);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
@@ -30,7 +112,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.maxDistance = 200;
-controls.minDistance = 20
+controls.minDistance = 20;
 
 // add resize listener
 window.addEventListener("resize", () => {
@@ -45,6 +127,5 @@ const renderloop = () => {
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderloop);
 };
-
 
 renderloop();
